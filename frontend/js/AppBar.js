7da60aff4e0ff7218e771293e884a5e6c0bbe853
Srinivas Reddy Thatiparthy <thatiparthysreenivas@gmail.com>
2019-12-30 20:09:23 +0530

    Fix hamburger icon

--- frontend/js/AppBar.js
+++ frontend/js/AppBar.js
@@ -1,12 +1,11 @@
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/core/MenuItem';
+import MenuIcon from '@material-ui/icons/Menu';
 import React from 'react';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-
 const useStyles = makeStyles((theme) => ({
   offset: theme.mixins.toolbar,
 }));
