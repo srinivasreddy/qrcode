346f32daffc5c6ad4ef4735d6fc8fd0af2868662
Srinivas Reddy Thatiparthy <thatiparthysreenivas@gmail.com>
2019-12-31 17:14:01 +0530

    Make migrations

--- backend/QR/models.py
+++ backend/QR/models.py
@@ -1,4 +1,12 @@
 from django.db import models
 
+from common.models import IndexedTimeStampedModel
 
-# Create your models here.
+
+class QRCode(IndexedTimeStampedModel, models.Model):
+    batch_number = models.CharField(max_length=100)
+    product_name = models.CharField(max_length=1000)
+    qr_code = models.BinaryField()
+    qr_code_svg = models.BinaryField()
+    qr_code_pdf = models.BinaryField()
+    qr_code_jpg = models.BinaryField()
