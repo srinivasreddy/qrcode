aa64d4d55ff8b96a7f494cc7731822049532de86
Srinivas Reddy Thatiparthy <thatiparthysreenivas@gmail.com>
2020-01-02 18:34:21 +0530

    rm

--- frontend/js/QRCodeSVG.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default class QRCodeSVG extends React.Component {
-
-  render() {
-    return (<div><p>Wilkommen</p></div>);
-    // <img alt="QRCodeSVG" className="images" src="asdasd" />;
-  }
-}
